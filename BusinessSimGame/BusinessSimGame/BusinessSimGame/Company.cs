2efd47abee3e17a310d3ddb3985d489ccfb8480b
Guardian008 <otonkin@ualberta.ca>
2013-10-06 02:37:06 +0100

    Added Class "Company"
    For now  it just has a basic formula for calculating the weekly  set of
    company values. We'll have to tweak it and add a lot but it's a start.

---
+++ BusinessSimGame/BusinessSimGame/BusinessSimGame/Company.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessSimGame
+{
+    class Company
+    {
+        public Company()
+        {
+
+        }
+
+        public int[] weeklyUpdate()
+        {
+            int[] values = new int[5];
+            int dayTrend;
+            int weekTrend = Random(0, 1);
+            if (weekTrend == 1)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    dayTrend = Random(-10, 10) + 2;
+                    dayTrend = (dayTrend / Math.Abs(dayTrend)) * ((dayTrend / 6) ^ 4 + 1);
+                    values[i] = dayTrend;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    dayTrend = Random(-10, 10) - 2;
+                    dayTrend = (dayTrend / Math.Abs(dayTrend)) * ((dayTrend / 6) ^ 4 + 1);
+                    values[i] = dayTrend;
+                }
+            }
+
+            return values;
+        }
+
+        private int Random(int p, int p_2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
